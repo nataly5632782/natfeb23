270bf88159c6fbbe8d0cef5144e580afd78f3e85
nataly5632782 <160240851+nataly5632782@users.noreply.github.com>
2024-02-23 08:49:28 -0500

    Add files via upload

---
+++ nat/main.py
@@ -0,0 +1,4 @@
+from persona import persona
+
+p=persona("nataly"," Rivera")
+print(p.obtenernombre()) #llamamos al metodo 
---
+++ nat/persona.py
@@ -0,0 +1,9 @@
+class persona:
+    def __init__(self,nombre,apellido):
+        self.nombre=nombre
+        self.apellido=apellido
+    def obtenernombre(self):
+        return f'mi nombre es {self.nombre} {self.apellido}'
+     #instanciamos la clase
+     
+
